ae70e8d919c5c31cf8df62ec5ebb113bdab8163a
veeresh <veerreshr@gmail.com>
2020-08-08 22:20:14 +0530

    Updates

---
+++ precache-manifest.557ebdbe2325d8e60fcc6b5b02cb5a8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6ddf92b9807de632fac934e200ab29",
+    "url": "/chart/index.html"
+  },
+  {
+    "revision": "2188f924d4c860b483a5",
+    "url": "/chart/static/css/main.8e07c9fd.chunk.css"
+  },
+  {
+    "revision": "153a6f271afb04c2f08c",
+    "url": "/chart/static/js/2.9cc8ab09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chart/static/js/2.9cc8ab09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2188f924d4c860b483a5",
+    "url": "/chart/static/js/main.00560fe0.chunk.js"
+  },
+  {
+    "revision": "f14a0b531ccecb5c22b8",
+    "url": "/chart/static/js/runtime-main.473dc410.js"
+  }
+]);
